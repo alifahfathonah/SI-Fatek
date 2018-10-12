3ea454ec2e9cfbea54b7012c0ebc025d0eef389f
cycl0ps <xnajoan@yahoo.com>
2018-10-12 20:21:40 +0800

    Update zerolevel2/controllers/Welcome.php

--- zerolevel2/controllers/Welcome.php
+++ zerolevel2/controllers/Welcome.php
@@ -9,12 +9,13 @@ class Welcome extends CI_Controller {
 	
 	public function index() {
 	
-		$data['jlh_mahasiswa'] = 13416;
-		$data['jlh_dosen'] = 209;
+		//$data['jlh_mahasiswa'] = 13416;
+		//$data['jlh_dosen'] = 209;
 
-		//$data['jlh_mahasiswa'] = $this->apicall->get(URL_API.'jumlah/mahasiswa/fakultas?kode=2')[0]->jumlah;
-		//$data['jlh_dosen'] =  $this->apicall->get(URL_API.'jumlah/pegawai/academic/fakultas?kode=13')[0]->jumlah;
+		$data['jlh_mahasiswa'] = $this->apicall->get(URL_API.'jumlah/mahasiswa/fakultas?kode=2')[0]->jumlah;
+		$data['jlh_dosen'] =  $this->apicall->get(URL_API.'jumlah/pegawai/academic/fakultas?kode=13')[0]->jumlah;
 
+		$this->output->cache(30);
 		$this->load->view("welcome",$data);
 	
 	}
